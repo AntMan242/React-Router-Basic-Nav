c572fd09231d189248e0a7fc4914c6ce578d2a60
anthony <anthony24.kwhomes@gmail.com>
2019-04-08 15:37:15 -0700

    Nav Bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import {Route} from 'react-router-dom';
 
 const App = () => (
+<div className='App'>
   <div>
     <Navigation />
   </div>
+
+<Route exact path='/' compponent={Home} />
+<Route path='/about' component={About} />
+<Route path='contact' component={Contact} />
+
+</div>
 );
+
+
 
 export default App;
